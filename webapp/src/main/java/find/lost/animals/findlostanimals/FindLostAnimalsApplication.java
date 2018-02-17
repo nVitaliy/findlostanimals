bb62f6e4f24912f001fc8fc673474e267f3b41bd
nVitaliy <vnedbaliuk@gmail.com>
2018-02-17 10:16:53 +0100

    create a project

---
+++ webapp/src/main/java/find/lost/animals/findlostanimals/FindLostAnimalsApplication.java
@@ -0,0 +1,12 @@
+package find.lost.animals.findlostanimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindLostAnimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindLostAnimalsApplication.class, args);
+	}
+}
---
+++ webapp/src/test/java/find/lost/animals/findlostanimals/FindLostAnimalsApplicationTests.java
@@ -0,0 +1,16 @@
+package find.lost.animals.findlostanimals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FindLostAnimalsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
