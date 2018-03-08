239e74ac594cbda5fbb89a23e9b68b8541942d49
nVitalii <vnedbaliuk@gmail.com>
2018-03-08 19:19:56 +0100

    add dialog for choose found or lost animal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,53 +1,16 @@
-import {CdkTableModule} from '@angular/cdk/table';
-import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+
 import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
+  MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSidenavModule, MatTabsModule,
   MatToolbarModule
 } from '@angular/material';
 import 'hammerjs';
 import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {AppComponent} from './app.component';
+import {AppComponent, DialogOverviewExampleDialog} from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     MatToolbarModule,
@@ -59,6 +22,8 @@ import {AppComponent} from './app.component';
     BrowserAnimationsModule,
     MatDialogModule
   ],
+  entryComponents: [AppComponent, DialogOverviewExampleDialog],
+  declarations: [AppComponent, DialogOverviewExampleDialog],
   providers: [],
   bootstrap: [AppComponent]
 })
